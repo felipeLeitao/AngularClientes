1f6c795345dd8c9a56e9e2031870572b3af0e19e
Felipe Leitão <fesilva@outlook.com.br>
2016-06-19 16:31:16 -0300

    controllers

---
+++ app/modules/clientes/controllers/cliente.listar.controller.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    'use strict';
+
+    //Pegando o módulo app (ele precisa já ter sido setado, se não da ruim)
+    angular.module('app').controller('clienteListarController', clienteController);
+
+    clienteController.$inject = ['$scope'];
+
+    function clienteController($scope) {
+
+        //Estou jogando a variavel de escopo dentro da variavel vm
+        //Você não pode fazer isso dentro de uma function interna
+        var vm = this;
+
+        vm.ola = "Olá mundo";
+    }
+})();
