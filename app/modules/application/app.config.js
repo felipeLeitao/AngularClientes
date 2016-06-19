7e002a8a9f4786e2cffe105750a4bc8e847fdf0d
Felipe Leitão <fesilva@outlook.com.br>
2016-06-19 15:53:49 -0300

    Rotas

---
+++ app/modules/application/app.config.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    'use strict';
+
+    //Pegando o módulo app (ele precisa já ter sido setado, se não da ruim)
+    angular.module('app').config(minhaConfig);
+
+    //Jeito Hackerzão de se injetar dependencias
+    minhaConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function minhaConfig($stateProvider, $urlRouterProvider)
+    {
+        //Aqui nesse comando definimos a rota padrão pra nossa aplicação
+        $urlRouterProvider.otherwise('/listar');
+
+        $stateProvider.state('main',
+            {
+                abstract: true,
+                templateUrl: 'modules/clientes/views/layout.html'
+            }).state('main.listar',
+            {
+                templateUrl: 'modules/clientes/views/listar.html',
+                url: '/listar'
+            }).state('main.cadastrar', {
+                templateUrl: 'modules/clientes/views/cadastrar.html',
+                url: '/cadastrar'
+            });
+    }
+})();
--- app/modules/application/app.module.js
+++ app/modules/application/app.module.js
@@ -1,1 +1,15 @@
-﻿
+﻿(function () {
+    //padrão IIFE
+
+    'use strict';
+    //Não deixa criar variavel sem o var a esquerda
+
+    //Getters and Setters
+
+    //Aqui criamos o nosso módulo principal e dizemos que ele vai precisar
+    //do módulo "ui.router"
+    angular.module('app',
+        [
+            'ui.router'
+        ]);
+})();
